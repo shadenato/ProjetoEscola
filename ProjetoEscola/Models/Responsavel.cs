3f91b5eb6bfe4898d42d0f8febac942e2706bb18
Renato <renatoaugustosa@gmail.com>
2025-05-28 20:06:52 -0300

    4. Codificando a Model Responsavel

--- ProjetoEscola/Models/Responsavel.cs
+++ ProjetoEscola/Models/Responsavel.cs
@@ -1,6 +1,13 @@
-﻿namespace ProjetoEscola.Models
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ProjetoEscola.Models
 {
     public class Responsavel
     {
+        public int CodResp { get; set; }
+        public int CodAluno { get; set; }
+        public string? Nome { get; set; }
+
+        public Aluno Aluno { get; set; }
     }
 }
